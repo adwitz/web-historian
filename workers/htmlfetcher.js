9abc7048890e6d82f2c50604fca4a3fdd706acb1
adwitz <adam.witzel@gmail.com>
2013-10-16 20:47:12 -0700

    Passes all specs. Successfully fetches root html files for sites in sites.txt using cron.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,13 @@
 // eventually, you'll have some code here that uses the tested helpers 
 // to actually download the urls you want to download.
+var path = require('path');
+
+var fetcherhelpers = require('./lib/html-fetcher-helpers');
+
+var crawl = function() {
+  fetcherhelpers.readUrls(path.resolve(__dirname, '../data/sites.txt'), function(urls) {
+      console.log('urlsArray', urls);
+      fetcherhelpers.downloadUrls(urls);
+    });
+};
+crawl();
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,17 +1,46 @@
 var fs = require('fs');
+var path = require('path');
+var http = require('http');
 
-exports.readUrls = function(filePath, cb){
+
+exports.readUrls = readUrls = function(filePath, cb){
   var sites;
   fs.readFile(filePath, "utf8", function(error, content){
     if (error){
       return;
     } else {
       sites = content.split('\n');
+      sites.splice(sites.length - 1, 1);
+      console.log(sites);
       cb(sites);
     }
   });
 };
 
 exports.downloadUrls = function(urls){
-  // fixme
+  for (var i = 0; i < urls.length; i++) { 
+    var filePath = path.resolve(__dirname, '../../data/sites/' + urls[i]);
+    var options = {
+      host: urls[i],
+      path: '/'
+    };
+    http.get(options, function(res) {
+      var data = '';
+      res.on('data', function(chunk){
+        data += chunk;
+      });
+      res.on('end', function(){
+        fs.writeFile(filePath, data, function(error) {
+          if (error) {
+            console.log('failed to write successfully');
+          } else {
+            console.log('wrote successfully');
+          }
+        });
+      });
+    });
+  }
 };
+
+
+
