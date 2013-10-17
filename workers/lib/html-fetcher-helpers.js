d8d8146a1813b9297ca5e44d87adf168b366f1a5
adwitz <adam.witzel@gmail.com>
2013-10-17 12:36:15 -0700

    working on adding to database

--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var http = require('http');
+var mysql = require('mysql');
 
 
 exports.readUrls = readUrls = function(filePath, cb){
@@ -18,29 +19,73 @@ exports.readUrls = readUrls = function(filePath, cb){
 };
 
 exports.downloadUrls = function(urls){
-  for (var i = 0; i < urls.length; i++) { 
-    var filePath = path.resolve(__dirname, '../../data/sites/' + urls[i]);
-    var options = {
-      host: urls[i],
-      path: '/'
-    };
-    http.get(options, function(res) {
-      var data = '';
-      res.on('data', function(chunk){
-        data += chunk;
-      });
-      res.on('end', function(){
-        fs.writeFile(filePath, data, function(error) {
-          if (error) {
-            console.log('failed to write successfully');
-          } else {
-            console.log('wrote successfully');
-          }
-        });
-      });
-    });
-  }
+  dbConnect(loopThruUrls, urls);
 };
 
+exports.dbWrite = dbWrite = function(url, data, connection) {
+  console.log('writing', url, 'to database');
+  connection.query("insert into archive (url, html, stamp) values ('" + url + "', '" + data + "', NOW())", function(error, rows) {
+    if (error) {
+      console.log('error inserting');
+    } else {
+      console.log('inserted successfully');
+      // connection.end(function(error) {
+      //   if (error) {
+      //     console.log('error terminating connection');
+      //   } else {
+      //     console.log('successfully terminated connection');
+      //   }
+      // });
+    }
+  });
+};
 
+exports.fileWrite = fileWrite = function(url, data) {
+  var filePath = path.resolve(__dirname, '../../data/sites/' + url);
+  fs.writeFile(filePath, data, function(error) {
+    if (error) {
+      console.log('failed to write successfully');
+    } else {
+      console.log('wrote successfully');
+    }
+  });
+};
+
+exports.initiateRequest = initiateRequest = function(url) {
+  var options = {
+    host: url,
+    path: '/'
+  };
+  http.get(options, function(res) {
+    var data = '';
+    res.on('data', function(chunk){
+      data += chunk;
+    });
+    res.on('end', function(){
+      //fileWrite(urls[i], data);
+      dbWrite(url, data);
+    });
+  });
+};
+
+exports.dbConnect = dbConnect = function(cb, urls) {
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'web_historian'
+  });
+  connection.connect(function(error) {
+    if (error) {
+      console.log('error connecting to DB');
+    } else {
+      cb(urls);
+    }
+  });
+};
+
+exports.loopThruUrls = loopThruUrls = function(urls) {
+  for (var i = 0; i < urls.length; i++) {
+    initiateRequest(urls[i]);
+  }
+};
 
