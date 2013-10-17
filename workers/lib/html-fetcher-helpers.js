cfea7a6979dc007b43b95b4bdfd22d7d9162d118
adwitz <adam.witzel@gmail.com>
2013-10-16 17:28:53 -0700

    passing readURLs spec

--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,5 +1,15 @@
-exports.readUrls = function(filePath, cb){
+var fs = require('fs');
 
+exports.readUrls = function(filePath, cb){
+  var sites;
+  fs.readFile(filePath, "utf8", function(error, content){
+    if (error){
+      return;
+    } else {
+      sites = content.split('\n');
+      cb(sites);
+    }
+  });
 };
 
 exports.downloadUrls = function(urls){
